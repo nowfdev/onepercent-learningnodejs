e10e1c6d0b8c5e58ad9cc4f1244598261ab00792
nowfdev <nowf.dev@gmail.com>
2025-06-18 01:48:41 +0700

    mvc routes and controller

--- src/app.js
+++ src/app.js
@@ -6,7 +6,11 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+const route = require("./routes/app.route");
+
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+app.use(express.json());
 // HTTP logger
 
 // app.use(morgan("combined"));
@@ -21,18 +25,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resource/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/search", (req, res) => {
-  console.log(req.query.q);
-  res.render("search");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
